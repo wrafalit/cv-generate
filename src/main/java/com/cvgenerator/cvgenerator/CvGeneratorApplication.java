f0da8e7de450d70b1eb97e8f3ef441699e236c63
wrafalit <wrafalit@gmail.com>
2023-11-20 19:39:33 +0100

    create project

---
+++ src/main/java/com/cvgenerator/cvgenerator/CvGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.cvgenerator.cvgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvgenerator/cvgenerator/CvGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvgenerator.cvgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
